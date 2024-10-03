400576c3efe8383f56d61c6edce02d8966bd3a6b
HOME-DOWNSTAIRS\PC <andytwoods@gmail.com>
2024-10-03 19:58:39 +0100

    scripts

---
+++ Assets/Scripts/BasicMode.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMode : MonoBehaviour
+{
+
+    public List<GameObject> AdvancedMenus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
